d9c980e4ba2ff235fea3f46fcdbc81e4f3e09629
Brian <besweeney@mix.wvu.edu>
2018-10-19 10:33:24 -0400

    finished bot base functionality

--- litDiscord/uncomfy_bot.py
+++ litDiscord/uncomfy_bot.py
@@ -11,20 +11,24 @@ client = commands.Bot(command_prefix=prefix, description=des)
 
 @commands.has_role('Admin')
 @client.command(pass_context=True)
-async def timeout(ctx, args, member: discord.Member):
+async def timeout(ctx, args, member: discord.User):
 	pattern2 = re.compile('([0-9]*[\.]*[0-9]*)')
 	if pattern2.match(args):
 		await client.send_message(member, "You have been muted for: " + args + " minutes")
 		allroles=member.roles
 		role = discord.utils.get(member.server.roles, name='Timeout')
-		for roler in allroles:
-			await client.remove_roles(member, roler)
-		await client.add_roles(member,role)
+		await client.replace_roles(member, role)
+		location = discord.utils.find(lambda x: x.name == 'SadBoiZone',member.server.channels)
+		await client.move_member(member, location)
 		await asyncio.sleep(float(args)*60)
 		await client.send_message(member, "Your done now")
-		await client.remove_roles(member,role)
+		#await client.remove_roles(member, role)
+		roleList = []
 		for roler in allroles:
-			await client.add_roles(member, roler)
+			roleList.append(discord.utils.get(member.server.roles, id=roler.id))
+
+		await client.replace_roles(member, *roleList)	
+		#await client.remove_roles(member, role)
 	else:
 		await client.say("You gotta say !mute TimeInMinutes @user")
 @client.event
@@ -35,16 +39,25 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
-    await client.process_commands(message) 
-    pattern = re.compile('([\w]*[\s]*)*([Nn]*[iI1]+[gG6]+[gG6]*[eE3]+[rR]+[sS5]*)([\w]*[\s]*)*')
+	await client.process_commands(message) 
+	pattern = re.compile('([\w]*[\s]*)*([Nn]*[iI1]+[gG6]+[gG6]*[eE3]+[rR]+[sS5]*)([\w]*[\s]*)*')
 
-    if pattern.match(message.content):
-        await client.delete_message(message)
-    if message.server is None:
-    	await client.send_message(message.author, "Don't talk to me I'm shy")
+	if pattern.match(message.content):
+		await client.delete_message(message)
+	if message.server is None:
+		try:
+			await client.send_message(message.author, "Don't talk to me I'm shy")
+		except:
+			pass
 
 @client.command(pass_context=True)
 async def hello(ctx):
         await client.say('world')
+@client.command(pass_context=True)
+async def sayHi(ctx):
+	await client.send_message(ctx.message.author, "Hey")
+@client.command(pass_context=True)
+async def sayhito(ctx, member: discord.User):
+	await client.send_message(member, "hi")
 
 client.run("NTAyNTI2NjY3NjI1ODU3MDI1.DqpPtQ._heZFO4le7dKzkCfcZL0NaUrIUo")
